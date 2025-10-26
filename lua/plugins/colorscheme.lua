a0e786a9f726e1a88b603fd12369dacbbc0303e7
egoRockU <gerwin.lagutan.dev@gmail.com>
2025-10-26 20:03:33 +0800

    updated to onedarker

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,19 @@
 return{
-  { 
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
+--  { 
+--    "olimorris/onedarkpro.nvim",
+--    priority = 1000, -- Ensure it loads first``
+--    config = function()
+--      vim.cmd("colorscheme onedark") -- set colorscheme
+--    end, 
+--  }
+
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
     config = function()
-      vim.cmd("colorscheme onedark") -- set colorscheme
-    end, 
+      require("onedark").setup { style = "darker" }
+      require("onedark").load()
+    end,
   }
-  
+
 }
