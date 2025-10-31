1eeba8def224715781ae7401ba1c9bc49fd6f14a
egoRockU <lagutan.gerwin.bscs2021@gmail.com>
2025-11-01 02:58:34 +0800

    added lsp config

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    -- You might add custom configurations here if needed, but for basic setup,
+    -- mason-lspconfig handles the default configurations.
+  },
+}
